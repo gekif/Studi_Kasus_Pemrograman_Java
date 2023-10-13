0c65527d8788c9e1fd9e3871099e898d49298072
FikarFFF <dzulfikar.maulana@gmail.com>
2023-10-13 20:34:29 +0700

    Char

---
+++ 01_TipeData_Variabel_Array/DemoVarChar.java
@@ -0,0 +1,11 @@
+public class DemoVarChar {
+    public static void main(String args[]) {
+        char ch1, ch2;
+
+        ch1 = 88;
+        ch2 = 'Y';
+
+        System.out.print("ch1 dan ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
