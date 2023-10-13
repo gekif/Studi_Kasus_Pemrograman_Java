d635959173325e70d2fc87270860aba70642d2a1
FikarFFF <dzulfikar.maulana@gmail.com>
2023-10-13 20:22:53 +0700

    Hitung Luas Lingkaran

---
+++ 01_TipeData_Variabel_Array/HitungLuasLingkaran.java
@@ -0,0 +1,12 @@
+public class HitungLuasLingkaran {
+    public static void main(String args[]) {
+        double pi, r, a;
+
+        r = 10.8;
+        pi =  3.1416;
+
+        a = pi  * r * r;
+
+        System.out.println("Luas lingkaran adalah " + a);
+    }
+}
