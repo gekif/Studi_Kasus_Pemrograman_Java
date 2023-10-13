97b3e44b62af3d3788530e73c39394d0b00b163f
FikarFFF <dzulfikar.maulana@gmail.com>
2023-10-13 20:38:57 +0700

    Char Seperti Integer

---
+++ 01_TipeData_Variabel_Array/CharSepertiInteger.java
@@ -0,0 +1,11 @@
+public class CharSepertiInteger {
+    public static void main(String args[]) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 memuat " + ch1);
+
+        ch1++;
+        System.out.println("ch1 sekarang adalah " + ch1);
+    }
+}
